1171b3f742a9abfa21f35c31b9c9ebd64f60b25f
Edward <2203743F@student.tp.edu.sg>
2023-11-14 22:23:14 +0800

    Implemented the backend for register

---
+++ models/User.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(fullname, employeeid, password) {
+        this.fullname = fullname;
+        this.employeeid = employeeid;
+        this.password = password;
+    }
+}
+
+module.exports = { User };
