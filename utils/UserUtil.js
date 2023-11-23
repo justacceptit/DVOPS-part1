31a701819d937e0023c1f2e08047c5b27d82bcc5
Edward <2203743F@student.tp.edu.sg>
2023-11-23 17:37:08 +0800

    Implemented the backend for login

--- utils/UserUtil.js
+++ utils/UserUtil.js
@@ -41,9 +41,32 @@ async function register(req, res) {
   }
 }
 
+async function login(req, res) {
+  try {
+  const fullname = req.body.fullname;
+  const password = req.body.password;
+  const allUsers = await readJSON('utils/users.json');
+  var validCredentials = false;
+  for (var i = 0; i < allUsers.length; i++) {
+  var currUser = allUsers[i];
+  if (currUser.fullname == fullname && currUser.password == password)
+  validCredentials = true;
+  }
+  if (validCredentials) {
+  return res.status(201).json({ message: 'Login successful!' });
+  } else {
+  return res.status(500).json({ message: 'Invalid credentials!' });
+  }
+  } catch (error) {
+  return res.status(500).json({ message: error.message });
+  }
+  }
+
 module.exports = {
   readJSON,
   writeJSON,
-  register
+  register,
+  login
 };
+
 
