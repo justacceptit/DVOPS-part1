a29a737f34211c2014ab6ca1c616dbb59a4e6861
justacceptit <2205099A@student.tp.edu.sg>
2023-11-25 15:47:18 +0800

    commented out add profile

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+const PORT = process.env.PORT || 5050
+var startPage = "index.html";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+const{addProfile}=require('./utils/EmployeeUtil')
+app.post('/add-profile',addProfile);
+const{editProfile}=require('./utils/EditProfile')
+app.put('/edit-profile/:id',editProfile);
+const{getProfile}=require('./utils/GetProfile')
+app.get('/get-profile',getProfile);
+app.get('/', (req, res) => {
+res.sendFile(__dirname + "/public/" + startPage);
+})
+app.listen(PORT, function () {
+console.log(`Demo project at: ${PORT}!`); });
