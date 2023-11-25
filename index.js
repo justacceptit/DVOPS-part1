f3cc500980435a2fc01c343b0871a3d0513ecef9
Edward <2203743F@student.tp.edu.sg>
2023-11-25 21:40:37 +0800

    Implemented the backend for register

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
+
 const PORT = process.env.PORT || 5050
 var startPage = "index.html";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const { register } = require('./utils/UserUtil')
+app.post('/register', register);
+
 app.get('/', (req, res) => {
 res.sendFile(__dirname + "/public/" + startPage);
 })
+
 app.listen(PORT, function () {
 console.log(`Demo project at: ${PORT}!`); });
