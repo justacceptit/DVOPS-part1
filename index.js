c36475c4ba9208c90f4ee290d67c575d43a52468
justacceptit <2205099A@student.tp.edu.sg>
2023-11-26 19:21:23 +0800

    kk

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
+
 const PORT = process.env.PORT || 5050
 var startPage = "index.html";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+<<<<<<< HEAD
 const{addProfile}=require('./utils/EmployeeUtil')
 app.post('/add-profile',addProfile);
+<<<<<<< HEAD
 const{getProfile}=require('./utils/GetProfile')
 app.get('/get-profile',getProfile);
 const{DayIn}=require('./utils/TimeIn')
 app.post('/time-in',DayIn);
+=======
+const{editProfile}=require('./utils/EditProfile')
+app.put('/edit-profile/:id',editProfile);
+//const { TimeIn } = require('./utils/TimeIn');
+//app.put('/time-in',TimeIn);
+const { EndOfDay } = require('./utils/EndOfDay');
+app.put('/eod',EndOfDay);
+=======
+
+
+const { deleteUser, register, login, updateUserTime } = require('./utils/UserUtil');
+
+// Define routes
+app.delete('/delete-user/:id', deleteUser);
+app.post('/register', register);
+app.post('/login', login);
+app.put('/update-user-time/:id', updateUserTime);
+
+
+>>>>>>> main
+>>>>>>> main
 app.get('/', (req, res) => {
-res.sendFile(__dirname + "/public/" + startPage);
-})
-app.listen(PORT, function () {
-console.log(`Demo project at: ${PORT}!`); });
+    res.sendFile(__dirname + "/public/" + startPage);
+});
+
+const server = app.listen(PORT, function () {
+    console.log(`Demo project at: ${PORT}!`);
+});
+
+module.exports = { app, server };
---
+++ utils/UserUtil.js
@@ -0,0 +1,141 @@
+const { User } = require('../models/User');
+const fs = require('fs').promises;
+
+async function readJSON(filename) {
+  try {
+    const data = await fs.readFile(filename, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function writeJSON(object, filename) {
+  try {
+    const allObjects = await readJSON(filename);
+    allObjects.push(object);
+    await fs.writeFile(filename, JSON.stringify(allObjects), 'utf8');
+    return allObjects;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+// Validation checks for register
+async function register(req, res) {
+  try {
+    const name = req.body.name; // Renamed from fullname to name
+    const password = req.body.password; // Password required
+
+    // Validation checks done for registration
+    if (name.length < 3 || password.length < 6) {
+      // Name must be more than 3 characters long, Password must be more than 6 characters
+      return res.status(500).json({ message: 'Validation error' });
+    } else {
+      const newUser = new User(name, password); // Renamed from fullname to name
+      const updatedUsers = await writeJSON(newUser, 'utils/users.json');
+      return res.status(201).json(updatedUsers);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+// Validation checks done for login
+async function login(req, res) {
+  try {
+    const name = req.body.name; // Renamed from fullname to name
+    const password = req.body.password;
+    const allUsers = await readJSON('utils/users.json');
+    var validCredentials = false;
+    
+    for (var i = 0; i < allUsers.length; i++) {
+      var currUser = allUsers[i];
+      if (currUser.name == name && currUser.password == password) {
+        // Must exist in the database
+        validCredentials = true;
+      }
+    }
+
+    if (validCredentials) {
+      return res.status(201).json({ message: 'Login successful!' }); // Log In is a success
+    } else {
+      return res.status(500).json({ message: 'Invalid credentials!' }); // Log In is not a success
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+async function deleteUser(req, res) {
+  try {
+    const id = req.params.id;
+
+    const allUsers = await readJSON('utils/users.json');
+
+    var index = -1;
+
+    for (var i = 0; i < allUsers.length; i++) {
+      var currUser = allUsers[i];
+      if (currUser.id == id)
+        index = i;
+    }
+
+    if (index != -1) {
+      allUsers.splice(index, 1);
+      await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+      return res.status(201).json({ message: 'User deleted successfully!' });
+    } else {
+      return res.status(500).json({ message: 'Error occurred, unable to delete!' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+async function updateUserTime(req, res) {
+    try {
+        const id = req.params.id;
+
+        const allUsers = await readJSON('utils/users.json');
+
+        const userIndex = allUsers.findIndex(user => user.id == id);
+
+        if (userIndex !== -1) {
+            const user = allUsers[userIndex];
+
+            // Check if the user has already timed in
+            if (user.time) {
+                return res.status(400).json({ message: 'User already timed in!' });
+            }
+
+            // Update the "time" property to the current time
+            user.time = new Date().toLocaleTimeString();
+
+            // Save the updated users array to the file
+            await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+
+            return res.status(200).json({ message: 'User time updated successfully!' });
+        } else {
+            return res.status(404).json({ message: 'User not found!' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+
+
+module.exports = {
+
+
+  readJSON,
+  writeJSON,
+  register,
+  login,
+  deleteUser,
+  updateUserTime
+};
+
