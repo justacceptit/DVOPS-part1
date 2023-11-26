deff289d545bdcfdc67cbfebfe06bb5aa3e47e53
proxyo1 <ryanlimxyy@gmail.com>
2023-11-26 16:20:12 +0800

    made changed to delete user function

--- public/js/home.js
+++ public/js/home.js
@@ -22,26 +22,5 @@ function deleteUser(selectedId) {
     request.send();
 }
 
-function updateUserTime(selectedId) {
-    var response = "";
 
-    var request = new XMLHttpRequest();
-
-    request.open("PUT", "/update-user-time/" + selectedId, true);
-    request.setRequestHeader('Content-Type', 'application/json');
-
-    request.onload = function () {
-        response = JSON.parse(request.responseText);
-
-        if (response.message == "User time updated successfully!") {
-            // Handle success, maybe redirect or display a message
-        }
-        else {
-            // Handle error, maybe show an alert
-            console.error('Unable to update user time!');
-        }
-    };
-
-    request.send();
-}
 
