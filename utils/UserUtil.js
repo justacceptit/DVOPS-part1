0a7235213707e7e1ab616ff208bba1e4a50a4142
ryan <83713328+proxyo1@users.noreply.github.com>
2023-11-26 17:53:22 +0800

    Revert "Done with delete user feature"

--- utils/UserUtil.js
+++ utils/UserUtil.js
@@ -22,19 +22,19 @@ async function writeJSON(object, filename) {
     throw err;
   }
 }
-
 // Validation checks for register
 async function register(req, res) {
   try {
-    const name = req.body.name; // Renamed from fullname to name
+    const fullname = req.body.fullname; // Full Name required
     const password = req.body.password; // Password required
 
+
     // Validation checks done for registration
-    if (name.length < 3 || password.length < 6) {
-      // Name must be more than 3 characters long, Password must be more than 6 characters
+    if (fullname.length < 3 || password.length < 6 ) { // Full Name must be more than 3 characters long, Password must be more than 6 characters
+
       return res.status(500).json({ message: 'Validation error' });
     } else {
-      const newUser = new User(name, password); // Renamed from fullname to name
+      const newUser = new User(fullname, password);
       const updatedUsers = await writeJSON(newUser, 'utils/users.json');
       return res.status(201).json(updatedUsers);
     }
@@ -42,36 +42,32 @@ async function register(req, res) {
     return res.status(500).json({ message: error.message });
   }
 }
-
-// Validation checks done for login
+  // Validation checks done for login
 async function login(req, res) {
   try {
-    const name = req.body.name; // Renamed from fullname to name
-    const password = req.body.password;
-    const allUsers = await readJSON('utils/users.json');
-    var validCredentials = false;
-    
-    for (var i = 0; i < allUsers.length; i++) {
-      var currUser = allUsers[i];
-      if (currUser.name == name && currUser.password == password) {
-        // Must exist in the database
-        validCredentials = true;
-      }
-    }
-
-    if (validCredentials) {
-      return res.status(201).json({ message: 'Login successful!' }); // Log In is a success
-    } else {
-      return res.status(500).json({ message: 'Invalid credentials!' }); // Log In is not a success
-    }
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
+  const fullname = req.body.fullname;
+  const password = req.body.password;
+  const allUsers = await readJSON('utils/users.json');
+  var validCredentials = false;
+  for (var i = 0; i < allUsers.length; i++) {
+  var currUser = allUsers[i];
+  if (currUser.fullname == fullname && currUser.password == password) // Must exist in the database
+  validCredentials = true;
   }
-}
+  if (validCredentials) {
+  return res.status(201).json({ message: 'Login successful!' }); // Log In is a success
+  } else {
+  return res.status(500).json({ message: 'Invalid credentials!' }); // Log In is not a success
+  }
+  } catch (error) {
+  return res.status(500).json({ message: error.message });
+  }
+  }
 
 module.exports = {
   readJSON,
   writeJSON,
   register,
-  login,
+  login
 };
+
