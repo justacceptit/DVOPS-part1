50132b55c6ba8fc98063ad978fca3e603d69f38e
proxyo1 <ryanlimxyy@gmail.com>
2023-11-26 16:14:49 +0800

    Implemented Time-in Feature

--- utils/UserUtil.js
+++ utils/UserUtil.js
@@ -5,31 +5,7 @@ const fs = require('fs').promises;
 
 
 
-async function deleteUser(req, res) {
-    try {
-        const id = req.params.id;
 
-        const allUsers = await readJSON('utils/users.json');
-
-        var index = -1;
-
-        for (var i = 0; i < allUsers.length; i++) {
-            var curcurrUser = allUsers[i];
-            if (curcurrUser.id == id)
-                index = i;
-        }
-
-        if (index != -1) {
-            allUsers.splice(index, 1);
-            await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
-            return res.status(201).json({ message: 'User deleted successfully!' });
-        } else {
-            return res.status(500).json({ message: 'Error occurred, unable to delete!' });
-        }
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
-    }
-}
 
 async function readJSON(filename) {
     try {
