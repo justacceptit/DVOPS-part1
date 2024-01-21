65aaaaa39ddc19684a58da992375c9e7f8efddaa
Edward <2203743F@student.tp.edu.sg>
2024-01-21 21:11:21 +0800

    Solved merge conflict on VS

--- utils/UserUtil.js
+++ utils/UserUtil.js
@@ -32,7 +32,7 @@ async function register(req, res) {
       const level=req.body.level;
       const time_in=req.body.time_in;
       const date=req.body.date;
-  if (name.length < 3 || password.length < 6) {
+  if (name.length < 3 || password.length < 6 || name.includes('@') || name.includes('#')) {
   return res.status(500).json({ message: 'Validation error' });
   } else {
   const newUser = new User(name, password,level,time_in,date);
@@ -73,32 +73,42 @@ async function login(req, res) {
 }
 
 async function deleteUser(req, res) {
-  try {
-    const id = req.params.id;
-
-    const allUsers = await readJSON('utils/users.json');
-
-    var index = -1;
-
-    for (var i = 0; i < allUsers.length; i++) {
-      var currUser = allUsers[i];
-      if (currUser.id == id)
-        index = i;
+    try {
+      const id = req.params.id;
+  
+      const allUsers = await readJSON('utils/users.json');
+  
+      const userIndex = allUsers.findIndex(user => user.id == id);
+  
+      if (userIndex !== -1) {
+        const userToDelete = allUsers[userIndex];
+  
+        // Check if the user has "level" as "2"
+        if (userToDelete.level === '2') {
+          const usersWithLevel2 = allUsers.filter(user => user.level === '2');
+  
+          // Check if the user to be deleted is the last user with "level" as "2"
+          if (usersWithLevel2.length === 1 && usersWithLevel2[0].id === userToDelete.id) {
+            return res.status(400).json({ message: 'Cannot delete the last user with level 2!' });
+          }
+        }
+  
+        allUsers.splice(userIndex, 1);
+        await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+        return res.status(201).json({ message: 'User deleted successfully!' });
+      } else {
+        return res.status(404).json({ message: 'User not found!' });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
     }
-
-    if (index != -1) {
-      allUsers.splice(index, 1);
-      await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
-      return res.status(201).json({ message: 'User deleted successfully!' });
-    } else {
-      return res.status(500).json({ message: 'Error occurred, unable to delete!' });
-    }
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
   }
-}
+  
+  
 
-async function updateUserTime(req, res) {
+  
+
+  async function updateUserTime(req, res) {
     try {
         const id = req.params.id;
 
@@ -110,12 +120,12 @@ async function updateUserTime(req, res) {
             const user = allUsers[userIndex];
 
             // Check if the user has already timed in
-            if (user.time) {
+            if (user.time_in) {
                 return res.status(400).json({ message: 'User already timed in!' });
             }
 
             // Update the "time" property to the current time
-            user.time = new Date().toLocaleTimeString();
+            user.time_in = new Date().toLocaleTimeString();
 
             // Save the updated users array to the file
             await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
@@ -129,6 +139,42 @@ async function updateUserTime(req, res) {
     }
 }
 
+async function updateUserTimeOut(req, res) {
+  try {
+    const id = req.params.id;
+
+    const allUsers = await readJSON('utils/users.json');
+
+    const userIndex = allUsers.findIndex(user => user.id == id);
+
+    if (userIndex !== -1) {
+      const user = allUsers[userIndex];
+
+      // Check if the user has not timed in
+      if (!user.time_in) {
+        return res.status(400).json({ message: 'User has not timed in yet!' });
+      }
+
+      // Check if the user has already timed out
+      if (user.time_out) {
+        return res.status(400).json({ message: 'User already timed out!' });
+      }
+
+      // Update the "time_out" property to the current time
+      user.time_out = new Date().toLocaleTimeString();
+
+      // Save the updated users array to the file
+      await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+
+      return res.status(200).json({ message: 'User time out updated successfully!' });
+    } else {
+      return res.status(404).json({ message: 'User not found!' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
 
 
 module.exports = {
@@ -139,6 +185,7 @@ module.exports = {
   register,
   login,
   deleteUser,
-  updateUserTime
+  updateUserTime,
+  updateUserTimeOut
 };
 
