93006607a94591356e12951011d105d328b63842
Edward <2203743F@student.tp.edu.sg>
2024-01-23 11:24:38 +0800

    Implemented session storage for level upon log in

--- public/js/login.js
+++ public/js/login.js
@@ -4,22 +4,23 @@ function login() {
     jsonData.name = document.getElementById("name").value;
     jsonData.password = document.getElementById("password").value;
     if (jsonData.name == "" || jsonData.password == "") {
-    document.getElementById("error").innerHTML = 'All fields are required!';
-    return;
+        document.getElementById("error").innerHTML = 'All fields are required!';
+        return;
     }
     var request = new XMLHttpRequest();
     request.open("POST", "/login", true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.onload = function() {
-    response = JSON.parse(request.responseText);
-    console.log(response)
-    if (response.message == "Login successful!") {
-    sessionStorage.setItem("name", jsonData.name);
-    window.location.href = 'home.html';
-    }
-    else {
-    document.getElementById("error").innerHTML = 'Invalid credentials!';
-    }
+        response = JSON.parse(request.responseText);
+        console.log(response)
+        if (response.message == "Login successful!") {
+            // Save user information in session storage
+            sessionStorage.setItem("name", jsonData.name);
+            sessionStorage.setItem("level", response.level); 
+            window.location.href = 'home.html';
+        } else {
+            document.getElementById("error").innerHTML = 'Invalid credentials!';
+        }
     };
     request.send(JSON.stringify(jsonData));
-    }
+}
