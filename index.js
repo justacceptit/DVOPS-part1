dc775fa99152658d882fce3129c3f0b1f56bd8e8
justacceptit <2205099A@student.tp.edu.sg>
2024-01-31 14:22:36 +0800

    completed the implementation of real time monitoring and logging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
-
+const logger = require('./logger');
 const PORT = process.env.PORT || 5050
 
 var startPage = "home.html";
@@ -40,10 +40,13 @@ app.get('/call/:name/:password',call);
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 });
-
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor());
 
 const server = app.listen(PORT, function () {
     console.log(`Demo project at: ${PORT}!`);
+    logger.info(`Demo project at: ${PORT}!`);
+logger.error(`Example or error log`)
 });
 
 module.exports = { app, server };
