edc9e0eabda79178e546510b4cd3c5ffcd099698
ryan <ryanlimxyy@gmail.com>
2024-02-01 17:06:05 +0800

    removed console.log statements to clean up npm test

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,6 @@
 function homePage(){
     window.location.href = 'home.html';
-    //console.log('HOMMMMMEEE')
+    ////console.log('HOMMMMMEEE')
 }
 function deleteUser(userId) {
     if (!userId) {
@@ -29,13 +29,13 @@ function deleteUser(userId) {
 
 function callname(){
     var value = sessionStorage.getItem('name');
-    console.log(value);
+    //console.log(value);
     return value;
 }
 
 function callpassword(){
     var value = sessionStorage.getItem('password');
-   console.log(value);
+   //console.log(value);
     return value;
 }
 
@@ -52,16 +52,16 @@ function call() {
 
         
         response = JSON.parse(request.responseText);
-            console.log('respose is ',response);
+            //console.log('respose is ',response);
         var level = response;    
         const splitt= response.split('/');
-        console.log(splitt)   
-        console.log(splitt[0])
+        //console.log(splitt)   
+        //console.log(splitt[0])
         sessionStorage.setItem("level", splitt[0])
         sessionStorage.setItem("id", splitt[1])    
 
            };
-           console.log('level:',sessionStorage.getItem('level'))
+           //console.log('level:',sessionStorage.getItem('level'))
     request.send();
 
 }
@@ -119,9 +119,9 @@ function callsession(){
             for (var i=0;i<len;i++){
             pass +='*'
             }
-            console.log(pass)
+            //console.log(pass)
         }else{
-        console.log(key + ": " + value)
+        //console.log(key + ": " + value)
        ;}
 
     });
@@ -136,7 +136,7 @@ function GetProfile() {
 
     request.onload = function () {
         response = JSON.parse(request.responseText);
-        console.log('lentght',response.length);
+        //console.log('lentght',response.length);
         var html = ''
         for (var i = 0; i < response.length; i++)
         if (response[i].id === '1') {
@@ -173,7 +173,7 @@ function GetProfileEdit() {
 
     request.onload = function () {
         response = JSON.parse(request.responseText);
-        console.log(response);
+        //console.log(response);
         var html = ''
         for (var i = 0; i < response.length; i++)
         if (response[i].id === '1000000') {
@@ -239,12 +239,12 @@ function GetProfileEdit() {
 
 function formatTimeForInput(timeString) {//headscratcher
     if(timeString=='Invalid Date'){
-        console.log("NO TIMEEEE" ,timeString)
+        //console.log("NO TIMEEEE" ,timeString)
         return '';
     }
     //splits the value eg. 12:32 == 12 ,32 with the : as seperator
     const parts = timeString.split(':');
-    //console.log(parts)
+    ////console.log(parts)
     if (parts.length === 3) {
         //padstart to check that there is a len of 2 for each parts
         return parts[0].padStart(2, '0') + ':' + parts[1].padStart(2, '0');
@@ -255,7 +255,7 @@ function formatTimeForInput(timeString) {//headscratcher
 }
 function formatDateForInput(dateString) {//headscratcher.2
     if (dateString == 'Invalid Date') {
-        console.log('no')
+        //console.log('no')
         return '';
     }
     const parts = dateString.split('/');
@@ -269,7 +269,7 @@ function formatDateForInput(dateString) {//headscratcher.2
 
 function editProfile(data) {
     var selectedProfile = JSON.parse(data);
-    //console.log(selectedProfile);
+    ////console.log(selectedProfile);
     //document.getElementById("editID").value = selectedProfile.id;
     document.getElementById("editName").value = selectedProfile.name;
     document.getElementById("editPassword").value = selectedProfile.password;
@@ -296,37 +296,37 @@ function editProfile(data) {
 }
 
 function updateProfile(id) {
-    // console.log(id)
+    // //console.log(id)
     var response = "";
-    //console.log(response);
+    ////console.log(response);
     var jsonData = new Object();
     jsonData.name = document.getElementById("editName").value;
     jsonData.password = document.getElementById("editPassword").value;
     jsonData.level = document.getElementById("editLevel").value;
     jsonData.date = document.getElementById("editDate").value;
     jsonData.time_in = document.getElementById("editTime_In").value;
-    //console.log('time',jsonData.time_in.length);
+    ////console.log('time',jsonData.time_in.length);
     //Format Time from 24 hr to 12 hr     
     const timeString = jsonData.time_in;//call the json data
     
-      //console.log('time',timeString.length);//error testing
+      ////console.log('time',timeString.length);//error testing
     if(!timeString==''||timeString.length!==5 ){
         const timeobjt = new Date('2023-12-01T' + timeString)//Uk locale parameters below
         const timeformat = timeobjt.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
         //the seconds is there to meet the dumb requirement for hh:mm:ss format
-        //  console.log(timeformat);//error testing
+        //  //console.log(timeformat);//error testing
         jsonData.time_in = timeformat;
     
     
     }
-    //console.log('time json',jsonData.time_in)
+    ////console.log('time json',jsonData.time_in)
     //Format Date from dd-mm-yyyy to dd/mm/yyyy
     const dateString = jsonData.date;//call the json data
-    //console.log('date',dateString)
+    ////console.log('date',dateString)
     if(!dateString==''){
         const dateobjt = new Date(dateString)//it didnt work when done in one line
         const dateformat = dateobjt.toLocaleDateString('en-GB');//Uk locale parameters
-        //console.log(dateformat);
+        ////console.log(dateformat);
         jsonData.date = dateformat;
     
     }
@@ -358,15 +358,15 @@ function updateProfile(id) {
 
     request.onload = function () {
         response = JSON.parse(request.responseText);
-        console.log(response)
+        //console.log(response)
         if (response.message == "Profile modified successfully!") {
             document.getElementById("editMessage").innerHTML = 'Edited Profile: ' + jsonData.name + '!';
             document.getElementById("editMessage").setAttribute("class", "text-success");
             window.location.href = 'edit.html';
-            console.log(jsonData.time_in);
+            //console.log(jsonData.time_in);
         }
         else {
-            console.log('No it didnt work')
+            //console.log('No it didnt work')
             document.getElementById("editMessage").innerHTML = 'Error occured,Unable to edit';
             document.getElementById("editMessage").setAttribute("class", "text-danger");
 
